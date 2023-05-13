640d7e26c3c67c632b195b06b2103ab81a732f22
Sergey Pugach <sergey.pugach02@gmail.com>
2023-05-13 05:00:14 +0300

    1

--- script.js
+++ script.js
@@ -158,8 +158,8 @@ function loop()
     for(var i = 0 ; i < num ; i++)
     {
         width = array[i+num];
-        lines1[i].style.width = width/2.5 + '%';
-        lines2[i].style.width = width/2.5 + '%';
+        lines1[i].style.width = width-150 + '%';
+        lines2[i].style.width = width-150 + '%';
 
         if(audioElement.paused)
         {
